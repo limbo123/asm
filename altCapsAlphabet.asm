08122527a819efd4804d0cad335755e34891e450
Ovramets <oleg.ovramets@teamvoy.com>
2023-05-24 01:56:06 +0300

    add comments

--- altCapsAlphabet.asm
+++ altCapsAlphabet.asm
@@ -2,43 +2,43 @@ section .text
   global _start
 
 _start:
-  mov cl, 65            ; char count
-  mov ch, 0             ; prev char
+  mov cl, 65              ; char count
+  mov ch, 0               ; prev char
   loop:
-      cmp cl, 91
+      cmp cl, 91          ; check if char is > 'Z'
       je finish
       cmp ch, cl
       jb printLowerCase
-      jmp printUpperCase 
-        
+      jmp printUpperCase
+
   printLowerCase:
       mov ch, 0
       add ch, cl
       add ch, 32
       add cl, 1
       jmp print
-	    
+
   printUpperCase:
       mov ch, cl
       add cl, 1
-      
+
   print:
       push cx
-      mov eax, 4           
-      mov ebx, 1           
+      mov eax, 4
+      mov ebx, 1
       mov [hello], ch
       mov edx, 1
       mov ecx, hello
       int 80h
       pop cx
       jmp loop
-      
+
   finish:
-      int 80h              
-      mov eax,1            
+      int 80h
+      mov eax,1
       mov ebx,0
       int 80h;
-  	
+
 section .data
   hello: db 'a', 1
 
